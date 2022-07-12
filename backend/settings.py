c184f10751931e89ad14877fb753d7703688fbb7
charliemday <45036245+charliemday@users.noreply.github.com>
2022-07-12 22:00:57 +0000

    Testing WhiteNoise

--- backend/settings.py
+++ backend/settings.py
@@ -203,10 +203,10 @@ AWS_LOCATION = 'static'
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory in production
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    # STATICFILES_STORAGE = "whi tenoise.storage.CompressedManifestStaticFilesStorage"
     # For media files, use the default Django storage backend.
     # DEFAULT_FILE_STORAGE = 'backend.storage.MediaStorage' 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
